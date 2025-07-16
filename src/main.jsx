7053a8a5bf18d776e3b5dab83ad61d6e3b560963
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-17 00:00:50 +0300

    editing vite configurations

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-import { render } from 'preact'
-import { App } from './app.jsx'
-import './index.css'
-
-render(<App />, document.getElementById('app'))
+import { render } from "preact";
+import App from "./app.jsx"; // ✅ Default import
+render(<App />, document.getElementById("app"));
