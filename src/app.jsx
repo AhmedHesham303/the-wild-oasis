75896b9e2a9c8567d128a6e045810944a0d12f74
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-27 15:04:41 +0300

    building some reusable components

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <StyledApp>
+      <H1>The wild oasis</H1>
+      <Button>click me </Button>
+      <Button>click him </Button>
+      <Input type="number" placeholder="number of guests"></Input>
+    </StyledApp>
+  );
 }
 
 export default App;
