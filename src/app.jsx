d6a792979991404d22fbaccbb9bfb84f19c6cccf
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-07-27 16:21:10 +0300

    connecting with supabase

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,7 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/globalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/input";
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import { BrowserRouter } from "react-router-dom";
 
-const StyledApp = styled.div`
-  background-color: orangered;
-  padding: 20px;
-`;
-
-function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <H1>The wild oasis</H1>
-        <Button>click me </Button>
-        <Button>click him </Button>
-        <Input type="number" placeholder="number of guests"></Input>
-      </StyledApp>
-    </>
-  );
+function app() {
+  return <BrowserRouter></BrowserRouter>;
 }
 
-export default App;
+export default app;
