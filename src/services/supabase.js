eca4bdc1a8ab9a72b5a0439e360e120c87483af1
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-08-05 15:44:24 +0300

    handel file upload and uploading images on supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://zwipdpfxynjidzvowfwu.supabase.co";
+export const supabaseUrl = "https://zwipdpfxynjidzvowfwu.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3aXBkcGZ4eW5qaWR6dm93Znd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDEyMjYsImV4cCI6MjA2ODE3NzIyNn0.NgUm1G8yHQWsenI72k1Y9u4BWJJZE4fTq8-eJaVP45U";
 const supabase = createClient(supabaseUrl, supabaseKey);
